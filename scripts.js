8274459e55e7a27a4f1b78a81dd457c147ebad6c
LucasR33 <lucasroux72@gmail.com>
2021-09-27 18:32:45 +0200

    Todo done

--- scripts.js
+++ scripts.js
@@ -1,6 +1,31 @@
+var todos = []
 
-const todosApp = {
 
+const TodoApp = {
+    data() {
+        return {
+            todos: window.todos,
+            newTodo : {
+                done: false
+            }
+        }
+    },
+    methods: {
+        doneTodo: function(item){
+            item.done = !item.done;
+        },
+        addTodo() {
+            this.todos.push(this.newTodo);
+            this.newTodo = {
+                done: false
+            }
+        },
+        clearTodo() {
+            this.todos = []
+        }
+    }
 }
 
-Vue.createApp(todosApp).mount('#app');
+
+
+Vue.createApp(TodoApp).mount("#app");
